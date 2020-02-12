b6541c50fb11435db54c2c3c7ea38c21e69833fa
Maria <murikmarusia@gmail.com>
2020-02-12 21:34:55 +0200

    Updates

---
+++ precache-manifest.5d2eeb0bb20c16ce55165d09cac963d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc273d95aeadd369f68bf7626bffd56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a98a36ec87352436205",
+    "url": "./static/css/main.a7874acc.chunk.css"
+  },
+  {
+    "revision": "5fdac0a2c9586d9d1584",
+    "url": "./static/js/2.6b91d4b6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.6b91d4b6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a98a36ec87352436205",
+    "url": "./static/js/main.942060dd.chunk.js"
+  },
+  {
+    "revision": "e67b79006193623cc7c8",
+    "url": "./static/js/runtime-main.a19c704a.js"
+  }
+]);
